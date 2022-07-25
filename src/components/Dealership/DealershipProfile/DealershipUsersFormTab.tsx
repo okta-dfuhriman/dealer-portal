a36fefcb76376c8c036f97c59ae4d2d59911e2d2
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-07-25 14:10:51 -0700

    refactor

--- src/components/Dealership/DealershipProfile/DealershipUsersFormTab.tsx
+++ src/components/Dealership/DealershipProfile/DealershipUsersFormTab.tsx
@@ -1,10 +1,24 @@
 import {
 	FormTab,
+	DateField,
+	Datagrid,
+	EditButton,
+	EmailField,
+	ReferenceManyField,
+	TextField,
 	useGetManyReference,
 	useRecordContext,
 	useTranslate,
 } from 'react-admin';
-import type { Dealership } from '.';
+import type { FormTabProps } from 'react-admin';
+
+import { UserLinkField } from 'components/User';
+
+export interface DeaershipUsersFormTabProps extends FormTabProps {
+	path?: string;
+	reference?: string;
+	target?: string;
+}
 
 const DealershipUsersFormTab = (props: any) => {
 	const record = useRecordContext<Dealership>();
@@ -17,7 +31,32 @@ const DealershipUsersFormTab = (props: any) => {
 	if (!isLoading) {
 		label += ` (${total})`;
 	}
-	return <FormTab label={label} {...props} />;
+	return (
+		<FormTab label={label} {...props}>
+			<ReferenceManyField reference='users' target='role'>
+				<Datagrid
+					sx={{
+						width: '100%',
+						'& .column-comment': {
+							maxWidth: '20em',
+							overflow: 'hidden',
+							textOverflow: 'ellipsis',
+							whiteSpace: 'nowrap',
+						},
+					}}
+				>
+					<UserLinkField />
+					<TextField source='status' />
+					<DateField source='created' label='Enrolled' />
+					<DateField source='lastLogin' label='Last Login' />
+					<EmailField source='profile.email' label='Email' />
+					<TextField source='profile.dealership' label='Dealership' />
+					<TextField source='profile.role' label='Role' />
+					<EditButton />
+				</Datagrid>
+			</ReferenceManyField>
+		</FormTab>
+	);
 };
 
 export default DealershipUsersFormTab;
