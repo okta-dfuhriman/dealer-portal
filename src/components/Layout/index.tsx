8fa9cf7dd527c9d41296866c352fb41669d18a0f
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-07-25 18:33:12 -0700

    add react-query devtools

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import { Layout, LayoutProps } from 'react-admin';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import AppBar from './AppBar';
 import Menu from './Menu';
 
 export default (props: LayoutProps) => {
-	return <Layout {...props} appBar={AppBar} menu={Menu} />;
+	return (
+		<>
+			<Layout {...props} appBar={AppBar} menu={Menu} />
+			<ReactQueryDevtools initialIsOpen={true} />
+		</>
+	);
 };
 
 export * from './themes';
