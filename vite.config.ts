b6b4412e777e5839b1895e5cdc3a55c94ed29247
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-08-10 11:39:49 -0700

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,30 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const defaultConfig = {
 	plugins: [react(), svgr(), tsconfigPaths()],
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+	if (mode === 'preview') {
+		return {
+			...defaultConfig,
+			build: {
+				minify: false,
+				target: 'esnext',
+			},
+		};
+	}
+
+	if (mode !== 'production') {
+		return {
+			...defaultConfig,
+			server: {
+				port: parseInt(process.env.PORT) || 3000,
+			},
+		};
+	}
+
+	return defaultConfig;
 });
